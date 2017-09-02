261dffb6e251138cd8ad7f523e2ccf52079bd950
markoidzan <marko.idzan@gmail.com>
2017-09-02 12:50:16 +0200

    Dang it
    Destrying site because of preloading of few weird javascripts -.-"

--- prefetch.php
+++ prefetch.php
@@ -10,25 +10,10 @@ Author URI: https://idzan.com.hr
 //* Adding DNS Prefetching
 function maskice_dns_prefetch() {
 echo '<meta http-equiv="x-dns-prefetch-control" content="on">
-<link rel="dns-prefetch" href="//secure.gravatar.com" />
-    <link rel="dns-prefetch" href="//www.google-analytics.com" />
-    <link rel="dns-prefetch" href="//apis.google.com" />
-    <link rel="dns-prefetch" href="//ad.doubleclick.net" />
-    <link rel="dns-prefetch" href="//pagead2.googlesyndication.com" />
-    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
-    <link rel="dns-prefetch" href="//fonts.googleapis.com" />
-    <link rel="dns-prefetch" href="//www.facebook.com" />
-    <link rel="dns-prefetch" href="//scontent.xx.fbcdn.net" />
-    <link rel="dns-prefetch" href="//gstatic.com" />
-    <link rel="dns-prefetch" href="//cdnjs.cloudflare.com" />
-    <link rel="dns-prefetch" href="//platform.twitter.com" />
-    <link rel="dns-prefetch" href="//jetpack.wordpress.com" />
-    <link rel="dns-prefetch" href="//s0.wp.com" />
-    <link rel="dns-prefetch" href="//s1.wp.com" />
-    <link rel="dns-prefetch" href="//s2.wp.com" />
-    <link rel="dns-prefetch" href="//public-api.wordpress.com" />
-    <link rel="dns-prefetch" href="//0.gravatar.com" />
-    <link rel="dns-prefetch" href="//1.gravatar.com" />
-    <link rel="dns-prefetch" href="//2.gravatar.com" />';';
+<link rel="dns-prefetch" href="//fonts.googleapis.com" />
+<link rel="dns-prefetch" href="//fonts.gstatic.com" />
+<link rel="dns-prefetch" href="//0.gravatar.com/" />
+<link rel="dns-prefetch" href="//2.gravatar.com/" />
+<link rel="dns-prefetch" href="//1.gravatar.com/" />';
 }
 add_action('wp_head', 'maskice_dns_prefetch', 0);
